aebd0172dfa89812f23456674ae9c0f9ff6609eb
moko <ljun925@naver.com>
2026-01-05 20:27:13 +0900

    chore: vercel analytics install

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import HeaderNav from '@/components/HeaderNav';
-
+import { Analytics } from '@vercel/analytics/next';
 export const metadata: Metadata = {
   title: 'Staff MOA',
   description: '스탭, 단기알바, 일일알바, 행사, 이벤트 구인 웹사이트',
@@ -17,6 +17,7 @@ export default function RootLayout({
       <body className="min-h-screen bg-accent text-foreground">
         <HeaderNav />
         <main>{children}</main>
+        <Analytics />
       </body>
     </html>
   );
