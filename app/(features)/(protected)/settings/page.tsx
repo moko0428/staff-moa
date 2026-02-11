bcdc25a11609596194b377d46ab4c255215de852
moko <ljun925@naver.com>
2026-02-11 17:56:45 +0900

    build 에러 해결

--- app/(features)/(protected)/settings/page.tsx
+++ app/(features)/(protected)/settings/page.tsx
@@ -15,7 +15,7 @@ import {
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from '@/components/ui/accordion';
+} from '@/app/components/ui/accordion';
 import {
   Dialog,
   DialogContent,
