7f3724c199be9860df1be0695a5d65bd430c7a96
moko <ljun925@naver.com>
2026-03-07 19:22:18 +0900

    fixed: 경로 에러 해결

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,5 @@
-export { default } from './(features)/(landing)/landing/page';
+import LandingPage from './(features)/(landing)/landing/page';
+
+export default function Home() {
+  return <LandingPage />;
+}
