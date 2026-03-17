078647572f449966f73b5e4d9336ce78ba9286c3
moko <ljun925@naver.com>
2026-03-17 14:38:33 +0900

    style: admin layout 추가

---
+++ app/(protected)/admin/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '고인력',
+  description: '스탭 구인은 고인력에서 시작됩니다.',
+};
+
+export default function AdminLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="bg-background">
+      <main className="max-w-7xl w-full mx-auto p-4">{children}</main>
+    </div>
+  );
+}
